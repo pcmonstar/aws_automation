978f9cbba3dee7e2edbf7c74b5588e0da6e44098
Prashant Choudhary <pcmonstar@gmail.com>
2026-09-10 21:04:04 +0530

    Add Terraform Config

---
+++ terraform/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = ">= 1.5.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "demo" {
+  bucket = "pcmonstar-github-actions-tf-demo-2026"
+}
